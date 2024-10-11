1031c159acd73fba2dc2db73bb1d52e58e1b8afc
Gustavo Caires <gustavocaires@hotmail.com.br>
2024-10-11 11:28:07 -0300

    integração com os os filmes nos card lateral

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,14 @@
-fetch('https://api.themoviedb.org/3/movie/tt12801262?language=pt-br&api_key=a2c2c30ca5fea3351982ff0d4436a2c6')
-.then( response => response.json())
-.then( data => {
+const API_KEY = 'a2c2c30ca5fea3351982ff0d4436a2c6'
+const API_LANGUAGE = 'pt-br'
+const BUTTON_PLAY = '<button type="button"><img src="/assets/icon-play-button.png" alt="Icon play"></button>'
+
+const LIST_MOVIES = ['tt12801262', 'tt4823776' ,'tt2096673', 'tt5109280', 'tt7146812', 'tt2948372', 'tt2953050', 'tt3521164', 'tt2380307', 'tt8097030']
+
+function getUrlMovie(movieId) {
+    return `https://api.themoviedb.org/3/movie/${movieId}?language=${API_LANGUAGE}&api_key=${API_KEY}`
+}
+
+fetch(getUrlMovie(LIST_MOVIES[0])).then( response => response.json()).then( data => {
     console.log(data)
     const app = document.getElementById('app')
 
@@ -19,3 +27,25 @@ fetch('https://api.themoviedb.org/3/movie/tt12801262?language=pt-br&api_key=a2c2
     const image = `https://image.tmdb.org/t/p/original${data.backdrop_path}`
     app.style.backgroundImage = `linear-gradient(90.18deg, rgba(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`
 })
+
+const moviesList = document.getElementById('movies_list')
+
+function createMovie(movieId) {
+    fetch(getUrlMovie(movieId)).then( response => response.json()).then( data => {
+        const movie = document.createElement('li')
+        const genre = `<span>${data.genres[0].name}</span>`
+        const title = `<strong>${data.title}</strong>`
+        const image = `https://image.tmdb.org/t/p/original${data.backdrop_path}`
+
+        movie.innerHTML = genre + title + BUTTON_PLAY
+        movie.style.backgroundImage = `linear-gradient(180deg, rgba(14, 23, 47, 0.0001) 11.72%, #0E172F 100%), url('${image}')`
+        moviesList.appendChild(movie)
+    })
+}
+
+function loadListMovies() {
+    LIST_MOVIES.map(createMovie)
+
+}
+
+loadListMovies()
