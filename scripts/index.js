97aaa851c700a3ff27781320f27e177242b425b6
Gustavo Caires <gustavocaires@hotmail.com.br>
2024-10-14 15:38:54 -0300

    cração da lista de filmes dinamica

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,52 @@
 const API_KEY = 'a2c2c30ca5fea3351982ff0d4436a2c6'
 const API_LANGUAGE = 'pt-br'
-const BUTTON_PLAY = '<button type="button"><img src="/assets/icon-play-button.png" alt="Icon play"></button>'
-
+const BASE_URL_IMAGE = 'https://image.tmdb.org/t/p/original'
 const LIST_MOVIES = ['tt12801262', 'tt4823776' ,'tt2096673', 'tt5109280', 'tt7146812', 'tt2948372', 'tt2953050', 'tt3521164', 'tt2380307', 'tt8097030']
+
+const moviesList = document.getElementById('movies_list')
 
 function getUrlMovie(movieId) {
     return `https://api.themoviedb.org/3/movie/${movieId}?language=${API_LANGUAGE}&api_key=${API_KEY}`
 }
 
-fetch(getUrlMovie(LIST_MOVIES[0])).then( response => response.json()).then( data => {
-    console.log(data)
-    const app = document.getElementById('app')
+function setMainMovie(movieId) {
+    fetch(getUrlMovie(movieId)).then( response => response.json()).then( data => {
+        console.log(data)
+        const app = document.getElementById('app')
+    
+        const title = document.querySelector('.movie h1')
+        const description = document.querySelector('.movie p')
+        const info = document.querySelector('.movie span')
+        const rating = document.querySelector('.rating strong')
+    
+        const yearRelease = data.release_date.split('-')[0]
+    
+        title.innerHTML = data.title
+        description.innerHTML = data.overview
+        rating.innerHTML = data.vote_average
+        info.innerHTML = yearRelease + ' - ' + data.genres[0].name + ' - Movie'
+    
+        const image = BASE_URL_IMAGE.concat(data.backdrop_path)
+        app.style.backgroundImage = `linear-gradient(90.18deg, rgba(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`
+    })
+}
 
-    const title = document.querySelector('.movie h1')
-    const description = document.querySelector('.movie p')
-    const info = document.querySelector('.movie span')
-    const rating = document.querySelector('.rating strong')
-
-    const yearRelease = data.release_date.split('-')[0]
-
-    title.innerHTML = data.title
-    description.innerHTML = data.overview
-    rating.innerHTML = data.vote_average
-    info.innerHTML = yearRelease + ' - ' + data.genres[0].name + ' - Movie'
-
-    const image = `https://image.tmdb.org/t/p/original${data.backdrop_path}`
-    app.style.backgroundImage = `linear-gradient(90.18deg, rgba(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`
-})
-
-const moviesList = document.getElementById('movies_list')
+function createButtonMovie(movieId) {
+    const button = document.createElement('button')
+    button.setAttribute('onclick', `setMainMovie('${movieId}')`)
+    button.innerHTML = '<img src="/assets/icon-play-button.png" alt="Icon play" />'
+    return button
+}
 
 function createMovie(movieId) {
     fetch(getUrlMovie(movieId)).then( response => response.json()).then( data => {
         const movie = document.createElement('li')
         const genre = `<span>${data.genres[0].name}</span>`
         const title = `<strong>${data.title}</strong>`
-        const image = `https://image.tmdb.org/t/p/original${data.backdrop_path}`
+        const image = BASE_URL_IMAGE.concat(data.backdrop_path)
 
-        movie.innerHTML = genre + title + BUTTON_PLAY
+        movie.innerHTML = genre + title
+        movie.appendChild(createButtonMovie(movieId))
         movie.style.backgroundImage = `linear-gradient(180deg, rgba(14, 23, 47, 0.0001) 11.72%, #0E172F 100%), url('${image}')`
         moviesList.appendChild(movie)
     })
@@ -49,3 +58,5 @@ function loadListMovies() {
 }
 
 loadListMovies()
+
+setMainMovie(LIST_MOVIES[0])
